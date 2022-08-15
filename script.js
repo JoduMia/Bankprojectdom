a713bf217d2b945bb5065c97a30335cf0ab5e476
ullah <sheikholeei97@gmail.ocm>
2022-08-16 01:54:23 +0600

    functionality added to the bank and all is well

--- script.js
+++ script.js
@@ -5,14 +5,49 @@ const mail = 'sheikhsuzan71@gmail.com';
 const pass = 223344;
 
 
+// LOgin function
 function sub() {
-
-    console.log('ilvoeu');
     if((email.value == mail) && (password.value == pass)) {
         return true;
     } else {
         alert('Couldn\'t recognize yourself. Input valid email and password !!!');
-        console.log(mail);
         return false;
     }
 }
+
+
+// update function
+
+function update() {
+    const depositBtn = document.getElementById('depositbtn');
+    const withdrawBtn = document.getElementById('withdrawbtn');
+    const balance = document.getElementById('balance');
+    const deposit = document.getElementById('depoupdate');
+    const withdraw = document.getElementById('withupdate');
+    const withamount = document.getElementById('withamount');
+    const dipoamount = document.getElementById('dipoamount');
+    let balances = 1200;
+    let withamo = 0;
+    let depositamount = 0;
+
+    depositBtn.addEventListener('click', function () {
+        balances = balances + Number(dipoamount.value);
+        balance.innerText = `$${balances}`;
+        depositamount = depositamount + Number(dipoamount.value);
+        deposit.innerText =`$${depositamount}`;
+    })
+
+    withdrawBtn.addEventListener('click', function () {
+        if((balances - Number(withamount.value)) >= 0) {
+            balances = balances - Number(withamount.value);
+            balance.innerText = `$${balances}`;
+            withamo = withamo + Number(withamount.value);
+            withdraw.innerText =`$${withamo}`;
+        } else {
+            alert('You have not enough balance to withdraw');
+        }
+    })
+
+}
+
+update();
